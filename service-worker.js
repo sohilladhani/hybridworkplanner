4eb5dfcba5dc28c54dc4ecc57ec5178b697f0e15
Sohil Ladhani <sladhani@salesforce.com>
2024-12-02 23:19:13 +0530

    Add PWA related changes

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'hybrid-work-planner-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/script.js',
+  '/icon-192.svg',
+  '/icon-512.svg',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
